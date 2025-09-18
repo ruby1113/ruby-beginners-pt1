73c2913ce5bce948db3654ebd677d6015610e6bb
Adrijan Petek <adrijangithub@gmail.com>
2025-09-18 12:13:59 +0200

    Add project files and CI workflow

---
+++ lib/tutorial/greeter.rb
@@ -0,0 +1,10 @@
+module Tutorial
+  class Greeter
+    def initialize(name)
+      @name = name
+    end
+    def greet
+      "Hello, #{@name}!"
+    end
+  end
+end
---
+++ spec/greeter_spec.rb
@@ -0,0 +1,7 @@
+require 'tutorial/greeter'
+RSpec.describe Tutorial::Greeter do
+  it 'greets by name' do
+    g = described_class.new('Ada')
+    expect(g.greet).to eq('Hello, Ada!')
+  end
+end
